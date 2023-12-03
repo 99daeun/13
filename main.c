5b1dc9e49d233cb845bcae5963cf3a2649f4e5a6
99daeun <lucy8631@naver.com>
2023-12-03 16:23:48 +0900

    2

--- main.c
+++ main.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-struct student {
-	int ID;//학번
-	char name[10];//이름
-	double grade;//학점
-	 
+struct point {
+    int x;
+    int y;
 };
 
-int main(void){
-	//instance 선언
-	struct student s1={123,"daeun",4.3}; 
-	//다른값 변경 
-	s1.ID=2311945;
-	strcpy(s1.name,"daeun");
-	s1.grade=4.3;
-	//출력
-	printf("ID:%d\n",s1.ID);
-	printf("name:%s\n",s1.name);
-	printf("grade:%f\n",s1.grade);
-	
-	return 0; 
+int main(int argc, char *argv[]) {
+    struct point p1, p2;
+    int xdiff, ydiff;
+    double dist;
+
+    printf("input p1 coordinate (x y) : ");
+    scanf("%d %d", &p1.x, &p1.y);
+
+    printf("input p2 coordinate (x y) : ");
+    scanf("%d %d", &p2.x, &p2.y); // Fixed the typo here
+
+    xdiff = p2.x - p1.x;
+    ydiff = p2.y - p1.y;
+    dist = sqrt(xdiff * xdiff + ydiff * ydiff);
+
+    printf("distance : %f\n", dist);
+
+    return 0;
 }
